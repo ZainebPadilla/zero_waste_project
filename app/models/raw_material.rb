137f2c07594cd440bec68f3a51c3b9b91527a559
Noravgz <Vergoznora@gmail.com>
2024-12-04 16:20:44 +0100

    add controller and model rawmaterials

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -1,2 +1,7 @@
 class RawMaterial < ApplicationRecord
-end
+    # Validations
+    validates :name, presence: true, uniqueness: true
+    validates :co2_per_kg, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :waste_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  end
+  
